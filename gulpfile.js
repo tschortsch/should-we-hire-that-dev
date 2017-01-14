969668ef7370fd06acda57fff92063f00266620d
Juerg Hunziker <juerg.hunziker@gmail.com>
2017-01-14 22:51:58 +0100

    add gulp tasks

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    //concat = require('gulp-concat'),
+    //uglify = require('gulp-uglify'),
+    notify = require('gulp-notify'),
+    sourcemaps = require('gulp-sourcemaps'),
+    autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('default', function () {
+});
+
+gulp.task('styles', function () {
+    return gulp.src('./assets/scss/styles.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }).on('error', sass.logError))
+        .pipe(autoprefixer('last 2 version'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./assets/css'))
+        .pipe(notify({message: 'Frontend styles task complete', onLast: true}));
+});
+
+gulp.task('scripts', function () {
+    return gulp.src([
+            './assets/external/jquery.easing/jquery.easing.js',
+            './assets/external/bootstrap-sass/assets/javascripts/bootstrap.js',
+            './assets/external/Leaflet/dist/leaflet-src.js',
+            './assets/external/Leaflet.markercluster/dist/leaflet.markercluster-src.js',
+            './assets/js/app.js'
+        ])
+        .pipe(concat('app.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./assets/js'))
+        .pipe(gulpif(!argv.headless, notify({message: 'Scripts task complete', onLast: true})));
+});
+
+gulp.task('deploy', [ 'styles', 'scripts' ]);
